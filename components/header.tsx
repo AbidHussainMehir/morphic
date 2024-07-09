7e1e147dcaab055c5e066e4df92bde94e12077a3
abid-dev1 <abid.dev1.fr@gmail.com>
2024-07-09 22:00:39 +0500

    changes in connect button model  also arrow button update clientId

--- components/header.tsx
+++ components/header.tsx
@@ -6,9 +6,35 @@ import { cn } from '@/lib/utils'
 import HistoryContainer from './history-container'
 import { ConnectButton, useActiveAccount } from 'thirdweb/react'
 import { client } from '../lib/utils/thirdweb-client'
+import {
+  ThirdwebProvider,
+  // ConnectButton,
+} from "thirdweb/react";
+import {
+  createWallet,
+  walletConnect,
+  inAppWallet,
+} from "thirdweb/wallets";
 export const Header: React.FC = () => {
   const account = useActiveAccount()
   let isConnected = !!account
+
+  const wallets = [
+    createWallet("io.metamask"),
+    createWallet("com.coinbase.wallet"),
+    walletConnect(),
+    inAppWallet({
+      auth: {
+        options: [
+          "email",
+          "google",
+          "apple",
+          "facebook",
+          "phone",
+        ],
+      },
+    }),
+  ];
   return (
     <header className="fixed w-full p-1 md:p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
       <div>
@@ -22,14 +48,27 @@ export const Header: React.FC = () => {
         <ModeToggle />
         <HistoryContainer location="header" />
         <div className={`${isConnected ? 'connected' : 'connect'}`}>
-          <ConnectButton
+        <ThirdwebProvider>
+      <ConnectButton
+        client={client}
+        wallets={wallets}
+        theme={"dark"}
+        connectModal={{
+          size: "compact",
+          title: "Connect to Athena AI",
+          titleIcon: "",
+          showThirdwebBranding: false,
+        }}
+      />
+    </ThirdwebProvider>
+          {/* <ConnectButton
             theme={'light'}
             client={client}
             appMetadata={{
               name: 'Athena',
               url: 'https://theathena.ai'
             }}
-          />
+          /> */}
         </div>
       </div>
     </header>
--- lib/utils/thirdweb-client.ts
+++ lib/utils/thirdweb-client.ts
@@ -1,5 +1,6 @@
 import { createThirdwebClient } from 'thirdweb'
 
-const CLIENT_ID = 'c8002c96c081491631b8c79d0a64f164'
+// const CLIENT_ID = 'c8002c96c081491631b8c79d0a64f164'
+const CLIENT_ID = 'bd17c7b6b06b460e770dbbb4edd673cf'
 
 export const client = createThirdwebClient({ clientId: CLIENT_ID })
